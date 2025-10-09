e6c1c73dbc80f40151c3814e941ab6e97f550532
YannAhlgrim <yann@ahlgrim.net>
2025-10-09 22:32:04 +0200

    dockerize frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8003',
   },
 }
 
